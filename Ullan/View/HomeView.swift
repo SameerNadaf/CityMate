e39bb80343bf99d5feec76881a9ca6eb382b0ac6
SameerNadaf <nsameernadaf@gmail.com>
2025-11-07 15:33:54 +0530

    Feat: Add search bar

--- Ullan/View/HomeView.swift
+++ Ullan/View/HomeView.swift
@@ -12,12 +12,21 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var shortLocation: UILabel!
     @IBOutlet weak var detailedLocation: UILabel!
     @IBOutlet weak var cartHeaderImageView: UIView!
+    @IBOutlet weak var searchBarView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Header
         cartHeaderImageView.layer.cornerRadius = cartHeaderImageView.frame.size.width / 2
         cartHeaderImageView.clipsToBounds = true
         cartHeaderImageView.layer.borderWidth = 2
         cartHeaderImageView.layer.borderColor = UIColor.lightGray.cgColor
+        
+        // search bar
+        searchBarView.layer.cornerRadius = 10
+        searchBarView.layer.borderWidth = 1
+        searchBarView.layer.borderColor = UIColor.lightGray.cgColor
+        searchBarView.clipsToBounds = true
     }
 }
